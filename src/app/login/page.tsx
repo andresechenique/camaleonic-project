7cbbc83e79dafd1c9a992e9de89bf87f090901c9
Andrés Echenique <andresechenique2002@gmail.com>
2025-09-15 13:06:57 +0200

    trying to fix login bug

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,74 +1,83 @@
-"use client";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
+'use client';
+import { AxiosError } from 'axios';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
 
 export default function Login() {
-  const [error, setError] = useState<string>();
-  const [loading, setLoading] = useState<boolean>(false);
-  const router = useRouter();
+	const [error, setError] = useState<string>();
+	const [loading, setLoading] = useState<boolean>(false);
+	const router = useRouter();
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoading(true);
-    setError(undefined);
-    const formData = new FormData(e.currentTarget);
-    const res = await signIn("credentials", {
-      email: formData.get("email"),
-      password: formData.get("password"),
-      redirect: false,
-    });
-    if (res?.error) {
-      setError(res.error as string);
-      setLoading(false);
-      return;
-    }
-    if (res?.ok) return router.push("/");
-    setLoading(false);
-  };
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setLoading(true);
+		setError(undefined);
+		const formData = new FormData(e.currentTarget);
+		const email = formData.get('email');
+		const password = formData.get('password');
+		try {
+			const res = await signIn('credentials', {
+				email,
+				password,
+				redirect: false,
+			});
 
-  return (
-    <div className="col-start-3 col-end-11 lg:col-start-5 lg:col-end-9">
-      <form
-        className="my-4 bg-gray-800 px-8 py-6 rounded-lg border border-gray-600"
-        onSubmit={handleSubmit}
-      >
-        <div className="flex flex-col items-center gap-4">
-          <h1 className="text-3xl font-semibold">Signin</h1>
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            className="bg-gray-700 rounded-lg p-3 placeholder-white min-w-full"
-            disabled={loading}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            className="bg-gray-700 rounded-lg p-3 placeholder-white min-w-full"
-            disabled={loading}
-          />
-          {error && <div className="text-red-400 text-xs">{error}</div>}
-          <button
-            type="submit"
-            className="px-6 py-2 bg-purple-700 rounded-lg min-w-full flex items-center justify-center"
-            disabled={loading}
-          >
-            {loading ? (
-              <span className="animate-spin rounded-full h-5 w-5 border-2 border-white border-t-transparent" />
-            ) : (
-              "Login"
-            )}
-          </button>
-          <div className="text-xs">
-            {"You don't have an account, "}
-            <a href="/register" className="hover:underline">
-              register here
-            </a>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+			if (res?.ok) return router.push('/');
+			setError('Authentication failed.');
+		} catch (error) {
+			if (error instanceof AxiosError) {
+				setError(error.response?.data.message);
+			} else {
+				setError('Something went wrong.');
+			}
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return (
+		<div className="col-start-3 col-end-11 lg:col-start-5 lg:col-end-9">
+			<form
+				className="my-4 bg-gray-800 px-8 py-6 rounded-lg border border-gray-600"
+				onSubmit={handleSubmit}
+			>
+				<div className="flex flex-col items-center gap-4">
+					<h1 className="text-3xl font-semibold">Signin</h1>
+					<input
+						type="email"
+						placeholder="Email"
+						name="email"
+						className="bg-gray-700 rounded-lg p-3 placeholder-white min-w-full"
+						disabled={loading}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						className="bg-gray-700 rounded-lg p-3 placeholder-white min-w-full"
+						disabled={loading}
+					/>
+					{error && <div className="text-red-400 text-xs">{error}</div>}
+					<button
+						type="submit"
+						className="px-6 py-2 bg-purple-700 rounded-lg min-w-full flex items-center justify-center"
+						disabled={loading}
+					>
+						{loading ? (
+							<span className="animate-spin rounded-full h-5 w-5 border-2 border-white border-t-transparent" />
+						) : (
+							'Login'
+						)}
+					</button>
+					<div className="text-xs">
+						{"You don't have an account, "}
+						<a href="/register" className="hover:underline">
+							register here
+						</a>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
 }
